ed182eeaa84a2b735abe02c2f99917323fe78867
Kristyn07 <kristinevernamoreno1@gmail.com>
2025-04-05 22:14:06 +0800

    WireFraming

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
