94d614ee8bd60fc0a613637d2ecdc6c28bb7e29d
Kristyn07 <kristinevernamoreno1@gmail.com>
2025-04-12 14:15:28 +0800

    Template Creation

--- app.py
+++ app.py
@@ -6,3 +6,35 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/important")
+def important():
+    return render_template("important.html")
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html")
+
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
+
+@app.route("/ongoing")
+def ongoing():
+    return render_template("ongoing.html")
+
+@app.route("/completed")
+def completed():
+    return render_template("completed.html")
+
+@app.route("/archieve")
+def archieve():
+    return render_template("archieve.html")
+
+@app.route("/trash")
+def trash():
+    return render_template("trash.html")
